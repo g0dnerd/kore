0121ba451ff3e6aa5268fa2ceeaedb4b0b735cd6
Paul Kukowski <kukowskipaul@gmail.com>
2026-01-25 13:16:12 +0100

    cleanup build files

--- src/args/build.zig
+++ src/args/build.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const test_step = b.step("test", "");
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -9,4 +10,14 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+
+    const tests = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/args.zig"),
+            .target = b.graph.host,
+        }),
+    });
+
+    const run_test = b.addRunArtifact(tests);
+    test_step.dependOn(&run_test.step);
 }
